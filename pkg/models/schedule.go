df7b3c77819ccce57a8e2fd5b161951bbf0692f1
Jaelse Ronald <jaelse.ronald@gmail.com>
2025-01-14 04:59:10 +0000

    Awesome retro styling and added At in schedule model

--- pkg/models/schedule.go
+++ pkg/models/schedule.go
@@ -1,10 +1,15 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Schedule struct {
 	gorm.Model
 	DropletID uint
 	Repeat    bool
 	IsDone    bool
+	At      time.Time
 }
